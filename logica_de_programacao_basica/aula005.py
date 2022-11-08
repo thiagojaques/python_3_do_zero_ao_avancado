131af4558f2a37f2cea46ef922f8b8e6dc723086
thiagohenriquedelimajaques@gmail.com <thiagohenriquedelimajaques@gmail.com>
2022-11-08 20:39:08 -0300

    adicionado aula004 e 005

---
+++ logica_de_programacao_basica/aula005.py
@@ -0,0 +1,12 @@
+# Váriaveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar: números e underline_ .
+# O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a um nome(variável).
+# Uso: nome_variavel = expressão
+
+#nome_completo = input('Digite seu nome completo: ')
+#print(nome_completo)
+
+nome = str(input('Digite seu nome completo: '))
+
+idade = int(input('Qual a sua idade: '))
+print(nome, idade)
